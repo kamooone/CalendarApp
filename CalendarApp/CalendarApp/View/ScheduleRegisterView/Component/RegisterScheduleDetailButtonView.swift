237054da0671bc89a10388bad14dc55efdb1d8e3
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-06-21 00:31:42 +0900

    スケジュール詳細をDBに登録したときに、エラーチェックを追加。

--- CalendarApp/CalendarApp/View/ScheduleRegisterView/Component/RegisterScheduleDetailButtonView.swift
+++ CalendarApp/CalendarApp/View/ScheduleRegisterView/Component/RegisterScheduleDetailButtonView.swift
@@ -10,11 +10,18 @@ import RealmSwift
 
 struct RegisterScheduleDetailButtonView: View {
     let scheduleDetailViewModel = ScheduleDetailViewModel.shared
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     
     var body: some View {
         
         Button(action: {
-            scheduleDetailViewModel.registerScheduleDetail()
+            if scheduleDetailViewModel.registerScheduleDetail() == 0 {
+                alertMessage = "登録が成功しました"
+            } else {
+                alertMessage = "登録に失敗しました"
+            }
+            showAlert = true
         }) {
             Text("登録")
                 .frame(width: 50, height: 30)
@@ -22,5 +29,12 @@ struct RegisterScheduleDetailButtonView: View {
         .offset(x:0,y:105)
         .buttonStyle(NormalButtonStyle.normalButtonStyle())
         .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("メッセージ"),
+                message: Text(alertMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        }
     }
 }
