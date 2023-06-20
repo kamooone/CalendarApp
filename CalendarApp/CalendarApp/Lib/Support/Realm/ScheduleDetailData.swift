9e1710cadcd8048cf899574d89e9288cadfba3b4
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-06-21 00:19:17 +0900

    スケジュール詳細のRealmのモデルを作成。

---
+++ CalendarApp/CalendarApp/Lib/Support/Realm/ScheduleDetailData.swift
@@ -0,0 +1,16 @@
+//
+//  TestRealm.swift
+//  CalendarApp
+//
+//  Created by Kazusa Kondo on 2023/06/15.
+//
+
+import Foundation
+
+struct ScheduleDetailData {
+    var scheduleDetailTitle = ""
+    var startTime = ""
+    var endTime = ""
+    var isNotice = true
+}
+
