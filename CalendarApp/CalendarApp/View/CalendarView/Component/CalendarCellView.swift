cbf92f11439dc1614017658afba85e28a2a175b7
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-07-13 23:35:30 +0900

    カレンダーのセルに登録したスケジュール詳細のデータ表示処理(仮)と、コード分割。

--- CalendarApp/CalendarApp/View/CalendarView/Component/CalendarCellView.swift
+++ CalendarApp/CalendarApp/View/CalendarView/Component/CalendarCellView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct CalendarCellView: View {
-    @EnvironmentObject var route: RouteObserver
     let calendarViewModel = CalendarViewModel.shared
     let scheduleDetailViewModel = ScheduleDetailViewModel.shared
     
@@ -57,74 +56,100 @@ struct CalendarCellView: View {
     
     var body: some View {
         VStack {
-            //7*6のマスを表示する
-            ForEach(0..<6, id: \.self) { week in
-                ZStack {
-                    HStack(spacing: 0) {
-                        ForEach(0..<7, id: \.self) { _ in
-                            Rectangle()
-                                .stroke(Color.black)
-                                .frame(width: 50, height: 80)
-                                .offset(x: 0, y: 0)
-                        }
-                    }
-                    
-                    // ToDo コードを分割させたい
-                    HStack(spacing: 0) {
-                        ForEach(1..<8, id: \.self) { i in
-                            if !(1..<calendarViewModel.numDaysMonth + 1).contains(i + week*7 - calendarViewModel.firstDayWeek.rawValue) {
-                                Text("")
+            if isRequestSuccessful {
+                //7*6のマスを表示する
+                ForEach(0..<6, id: \.self) { week in
+                    ZStack {
+                        HStack(spacing: 0) {
+                            ForEach(0..<7, id: \.self) { _ in
+                                Rectangle()
+                                    .stroke(Color.black)
                                     .frame(width: 50, height: 80)
-                            } else {
-                                Button(action: {
-                                    print("\(i+week*7 - calendarViewModel.firstDayWeek.rawValue)日をタップ")
-                                    
-                                    // ルートをスケジュール登録に変更して、選択したセルの月日を取得
-                                    route.path = .ScheduleConfirm
-                                    calendarViewModel.selectMonth = calendarViewModel.selectMonth + calendarViewModel.currentMonth
-                                    calendarViewModel.selectDay = i+week*7 - calendarViewModel.firstDayWeek.rawValue
-                                }) {
-                                    Text("\(i + (week*7) - calendarViewModel.firstDayWeek.rawValue)")
-                                        .frame(width: 50, height: 80)
-                                        .foregroundColor((i+week*7) == 1 || (i+week*7) == 8 || (i+week*7) == 15 || (i+week*7) == 22 || (i+week*7) == 29 || (i+week*7) == 36 ? Color.red : ((i+week*7) == 7 || (i+week*7) == 14 || (i+week*7) == 21 || (i+week*7) == 28 || (i+week*7) == 35 ? Color.blue : Color.black))
-                                        .offset(x: 0, y: -30)
-                                }
-                                .frame(width: 50, height: 80)
-                                .offset(x: 0, y: 0)
+                                    .offset(x: 0, y: 0)
+                            }
+                        }
+                        
+                        HStack(spacing: 0) {
+                            ForEach(1..<8, id: \.self) { i in
+                                DayStringtView(i: i, week: week)
                             }
                         }
                     }
+                    .padding(.bottom, -8)
+                    .offset(x: 0, y: -70)
                 }
-                .padding(.bottom, -8)
-                .offset(x: 0, y: -70)
-            }
-            
-            // ToDo スケジュール詳細をセルに表示させる
-            ForEach(0..<6, id: \.self) { week in
-                ZStack {
-                    HStack(spacing: 0) {
-                        ForEach(1..<8, id: \.self) { i in
-                            if !(1..<calendarViewModel.numDaysMonth + 1).contains(i + week*7 - calendarViewModel.firstDayWeek.rawValue) {
-                                Text("")
-                                    .frame(width: 50, height: 80)
-                            } else {
-                                // ToDo 表示させる文字は一つの詳細スケジュールにつき6文字まで、二つ目以降は改行して代入して、一つのStringにする
-                                // ToDo Text()にscheduleDetailMonthList[]を使用して日付に該当する内容を表示させる
-                                Text("テストテスト\nテストテスト\nテストテスト\nテストテスト\nテストテスト\nテストテスト")
-                                    .font(.system(size: 7))
-                                    .frame(width: 50, height: 80)
-                                    .foregroundColor(Color.black) // ToDo 大事な予定には色を付けれるようにすればいいかも
-                                    .offset(x: 0, y: -470)
+                
+                // ToDo スケジュール詳細をセルに表示させる
+                ForEach(0..<6, id: \.self) { week in
+                    ZStack {
+                        HStack(spacing: 0) {
+                            ForEach(1..<8, id: \.self) { i in
+                                CellTextView(i: i, week: week)
                             }
                         }
                     }
+                    .padding(.bottom, -8)
+                    .offset(x: 0, y: -70)
                 }
-                .padding(.bottom, -8)
-                .offset(x: 0, y: -70)
             }
         }
         .onAppear {
             bindViewModel()
+        }
+    }
+}
+
+
+struct DayStringtView: View {
+    @EnvironmentObject var route: RouteObserver
+    let calendarViewModel = CalendarViewModel.shared
+    let scheduleDetailViewModel = ScheduleDetailViewModel.shared
+    let i: Int
+    let week: Int
+    
+    var body: some View {
+        if !(1..<calendarViewModel.numDaysMonth + 1).contains(i + week*7 - calendarViewModel.firstDayWeek.rawValue) {
+            Text("")
+                .frame(width: 50, height: 80)
+        } else {
+            Button(action: {
+                print("\(i+week*7 - calendarViewModel.firstDayWeek.rawValue)日をタップ")
+                
+                // ルートをスケジュール登録に変更して、選択したセルの月日を取得
+                route.path = .ScheduleConfirm
+                calendarViewModel.selectMonth = calendarViewModel.selectMonth + calendarViewModel.currentMonth
+                calendarViewModel.selectDay = i+week*7 - calendarViewModel.firstDayWeek.rawValue
+            }) {
+                Text("\(i + (week*7) - calendarViewModel.firstDayWeek.rawValue)")
+                    .frame(width: 50, height: 80)
+                    .foregroundColor((i+week*7) == 1 || (i+week*7) == 8 || (i+week*7) == 15 || (i+week*7) == 22 || (i+week*7) == 29 || (i+week*7) == 36 ? Color.red : ((i+week*7) == 7 || (i+week*7) == 14 || (i+week*7) == 21 || (i+week*7) == 28 || (i+week*7) == 35 ? Color.blue : Color.black))
+                    .offset(x: 0, y: -30)
+            }
+            .frame(width: 50, height: 80)
+            .offset(x: 0, y: 0)
+        }
+    }
+}
+
+
+struct CellTextView: View {
+    let calendarViewModel = CalendarViewModel.shared
+    let scheduleDetailViewModel = ScheduleDetailViewModel.shared
+    let i: Int
+    let week: Int
+    
+    var body: some View {
+        if !(1..<calendarViewModel.numDaysMonth + 1).contains(i + week*7 - calendarViewModel.firstDayWeek.rawValue) {
+            Text("")
+                .frame(width: 50, height: 80)
+        } else {
+            // ToDo 表示させる文字は一つの詳細スケジュールにつき6文字まで、二つ目以降は改行して代入して、一つのStringにする
+            // ToDo Text()にscheduleDetailMonthList[]を使用して日付に該当する内容を表示させる
+            Text("\(scheduleDetailViewModel.scheduleDetailMonthList[1])")
+                .font(.system(size: 7))
+                .frame(width: 50, height: 80)
+                .foregroundColor(Color.black) // ToDo 大事な予定には色を付けれるようにすればいいかも
+                .offset(x: 0, y: -470)
         }
     }
 }
