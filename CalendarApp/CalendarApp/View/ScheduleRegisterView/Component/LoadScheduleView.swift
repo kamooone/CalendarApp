10a150a6ef9d4db41e976a13ec6f252ef408684e
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-07-23 16:31:41 +0900

    登録してある理想のスケジュールに差し替え。

--- CalendarApp/CalendarApp/View/ScheduleRegisterView/Component/LoadScheduleView.swift
+++ CalendarApp/CalendarApp/View/ScheduleRegisterView/Component/LoadScheduleView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct LoadScheduleView: View {
+    let scheduleDetailViewModel = ScheduleDetailViewModel.shared
+    
     @State private var selectedOption = 0
-    // ToDo 登録してある理想のスケジュールに差し替える
-    let options = ["-------------------", "作成した理想のスケ1", "作成した理想のスケ2", "作成した理想のスケ3"]
+    @State private var options: [String] = ["---------------"]
+    @State private var isRequestSuccessful = false
+    
+    func bindViewModel() {
+        isRequestSuccessful = false
+        
+        let group = DispatchGroup()
+        group.enter()
+        
+        DispatchQueue(label: "realm").async {
+            scheduleDetailViewModel.getIdealScheduleTitle { success in
+                group.leave()
+                
+                if success {
+                    print("非同期処理成功")
+                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
+                    DispatchQueue.main.async {
+                        isRequestSuccessful = true
+                        options.append(contentsOf: scheduleDetailViewModel.idealScheduleTitleArray)
+                        selectedOption = 0
+                    }
+                } else {
+                    print("非同期処理失敗")
+                    // ToDo 取得失敗エラーアラート表示
+                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
+                    DispatchQueue.main.async {
+                        isRequestSuccessful = false
+                    }
+                }
+            }
+        }
+        
+        // 成功失敗に関わらず呼ばれる
+        group.notify(queue: .main) {
+            // ToDo 失敗エラーアラート表示
+            print("非同期処理終了")
+        }
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -23,29 +61,34 @@ struct LoadScheduleView: View {
                 Spacer()
             }
             
-            HStack {
-                Spacer()
-                Picker("Select an option", selection: $selectedOption) {
-                    ForEach(0..<options.count, id: \.self) { index in
-                        Text(options[index])
+            if isRequestSuccessful {
+                HStack {
+                    Spacer()
+                    Picker("Select an option", selection: $selectedOption) {
+                        ForEach(0..<options.count, id: \.self) { index in
+                            Text(options[index])
+                        }
                     }
-                }
-                .frame(width: geometry.size.width * 0.6, height: geometry.size.height / 10)
-                .pickerStyle(MenuPickerStyle())
-                .offset(x:0,y:25)
-                
-                Button(action: {
+                    .frame(width: geometry.size.width * 0.6, height: geometry.size.height / 10)
+                    .pickerStyle(MenuPickerStyle())
+                    .offset(x:0,y:25)
                     
-                }) {
-                    Text("設定する")
-                        .frame(width: 80, height: 30)
+                    Button(action: {
+                        
+                    }) {
+                        Text("設定する")
+                            .frame(width: 80, height: 30)
+                    }
+                    .buttonStyle(NormalButtonStyle.normalButtonStyle())
+                    .padding() // ボタンの余白を調整
+                    .offset(x:0,y:20)
+                    Spacer()
                 }
-                .buttonStyle(NormalButtonStyle.normalButtonStyle())
-                .padding() // ボタンの余白を調整
-                .offset(x:0,y:20)
                 Spacer()
             }
-            Spacer()
+        }
+        .onAppear{
+            bindViewModel()
         }
     }
 }
