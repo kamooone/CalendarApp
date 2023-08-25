6be9a1402d60a44604768d2521c94a3ef4f1050d
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-08-26 00:32:57 +0900

    登録済みの理想のスケジュールの編集の際、編集ボタンを押した時にbindViewModelをするのではなく、確認画面を開く直前にbindViewModelをするように修正。

--- CalendarApp/CalendarApp/View/IdealScheduleTitleView/Component/EditIdealScheduleButtonView.swift
+++ CalendarApp/CalendarApp/View/IdealScheduleTitleView/Component/EditIdealScheduleButtonView.swift
@@ -16,7 +16,7 @@ struct EditIdealScheduleButtonView: View {
         Button(action: {
             scheduleDetailViewModel.idealScheduleTitle = _titleStr
             scheduleDetailViewModel.isIdealScheduleUpdate = true
-            bindViewModel()
+            route.path = .IdealSchedule
         }) {
             Text("修正")
                 .font(.system(size: 12))
@@ -25,37 +25,5 @@ struct EditIdealScheduleButtonView: View {
         .offset(x: 30, y: 0)
         .buttonStyle(NormalButtonStyle.normalButtonStyle())
         .padding()
-    }
-    
-    func bindViewModel() {
-        let group = DispatchGroup()
-        group.enter()
-        
-        DispatchQueue(label: "realm").async {
-            scheduleDetailViewModel.getIdealScheduleDetail { success in
-                group.leave()
-                
-                if success {
-                    print("非同期処理成功")
-                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
-                    DispatchQueue.main.async {
-                        route.path = .IdealSchedule
-                    }
-                } else {
-                    print("非同期処理失敗")
-                    // ToDo 取得失敗エラーアラート表示
-                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
-                    DispatchQueue.main.async {
-                        
-                    }
-                }
-            }
-        }
-        
-        // 成功失敗に関わらず呼ばれる
-        group.notify(queue: .main) {
-            // ToDo 失敗エラーアラート表示
-            print("非同期処理終了")
-        }
     }
 }
