bd174138ac6cdbee9bee8b92a6cad982d06e3188
Kazusa Kondo <hahaha.kazusa@gmail.com>
2023-08-26 00:49:43 +0900

    理想のスケジュール登録の際は、新規登録と更新で別の関数に分けていたが一つの関数にしたので不要な関数を削除。

--- CalendarApp/CalendarApp/View/IdealScheduleRegistView/Component/IdealAppendScheduleDetailButtonView.swift
+++ CalendarApp/CalendarApp/View/IdealScheduleRegistView/Component/IdealAppendScheduleDetailButtonView.swift
@@ -19,11 +19,7 @@ struct IdealAppendScheduleDetailButtonView: View {
             // ToDo タイトル未入力だとエラー表示させる。時間の設定がおかしい時もエラー表示させる。
             // ToDo 登録完了したら入力内容を初期化する
             Button(action: {
-                if scheduleDetailViewModel.isIdealScheduleUpdate {
-                    update()
-                } else {
-                    regist()
-                }
+                regist()
             }) {
                 Text("追加")
                     .frame(width: screenSizeObject.screenSize.width / 8, height: screenSizeObject.screenSize.height / 20)
@@ -52,39 +48,6 @@ struct IdealAppendScheduleDetailButtonView: View {
         
         DispatchQueue(label: "realm").async {
             scheduleDetailViewModel.registerIdealScheduleDetail { success in
-                group.leave()
-                
-                if success {
-                    print("非同期処理成功")
-                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
-                    DispatchQueue.main.async {
-                        alertMessage = "登録が成功しました"
-                    }
-                } else {
-                    print("非同期処理失敗")
-                    // メインスレッド（UI スレッド）で非同期に実行するメソッド
-                    DispatchQueue.main.async {
-                        alertMessage = "登録に失敗しました"
-                    }
-                }
-            }
-        }
-        
-        // 成功失敗に関わらず呼ばれる
-        group.notify(queue: .main) {
-            // ToDo 失敗エラーアラート表示
-            print("非同期処理終了")
-            showAlert = true
-        }
-    }
-    
-    func update() {
-        let group = DispatchGroup()
-        group.enter()
-        
-        DispatchQueue(label: "realm").async {
-            // ToDo 理想のスケジュール用の登録処理修正(登録済みのスケジュールは新規登録ではなくて更新になるようにする)
-            scheduleDetailViewModel.updateIdealScheduleDetail { success in
                 group.leave()
                 
                 if success {
